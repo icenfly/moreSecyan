d152bc3ad671c09fa17162c157c895d4039d6eaf
icenfly <87740812+icenfly@users.noreply.github.com>
2025-03-06 21:29:48 +0800

    fix ade9

--- src/core/relation.cpp
+++ src/core/relation.cpp
@@ -222,17 +222,17 @@ namespace SECYAN
 		float f_value;
 		const int arrlen = sizeof(uint64_t) / sizeof(char);
 		char padded_str[arrlen + 1] = "";
-		std::cout << "child relation: " << std::endl;
 		for (uint32_t i = 0; i < child.m_RI.numRows; i++)
 		{
-			//if (child.m_AI.knownByOwner && (child.m_Annot[i] == 0 && child.m_Tuples[i].IsDummy()))
-			//	continue;
+			if (child.m_AI.knownByOwner && (child.m_Annot[i] == 0 && child.m_Tuples[i].IsDummy()))
+				continue;
 			printed++;
 			std::cout << i + 1 << '\t';
 			if (!dummy)
 			{
 				for (uint32_t j = 0; j < child.m_RI.attrNames.size(); ++j)
 				{
+					std::cout << "child relation: " << std::endl;
 					switch (child.m_RI.attrTypes[j])
 					{
 					case DataType::INT:
