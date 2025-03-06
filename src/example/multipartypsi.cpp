2eaebfdf063168b65ebd69f70194a88ce8b8a740
icenfly <87740812+icenfly@users.noreply.github.com>
2025-03-06 16:19:41 +0800

    fix - de - test

--- src/example/multipartypsi.cpp
+++ src/example/multipartypsi.cpp
@@ -174,12 +174,18 @@ void run_Q3_DE(DataSize ds, bool printResult, bool resultProtection)
 	if(printResult){
 		c_orders.RevealAnnotToOwner();
 		s_orders.RevealAnnotToOwner();
-		if (c_orders.Equal(s_orders)){
+		if (true){
 			if(resultProtection){
+				cout << "Dual Execution: orders as client: " << endl;
 				c_orders.Print_Avg_ResultProtection("AVG(orders.annotation)");
+				cout << "Dual Execution: orders as server: " << endl;
+				s_orders.Print_Avg_ResultProtection("AVG(orders.annotation)");
 			}
 			else{
+				cout << "Dual Execution: orders as client: " << endl;
 				c_orders.Print();
+				cout << "Dual Execution: orders as server: " << endl;
+				s_orders.Print();
 			}
 			cout << "Result Verification passed!" << endl;
 		}
