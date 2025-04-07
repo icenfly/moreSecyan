7f8708cb15a7751fdee6b1a412b438b9ba943c19
icenfly <87740812+icenfly@users.noreply.github.com>
2025-04-07 16:21:54 +0800

    mod run query code structure

--- src/example/TPCH.cpp
+++ src/example/TPCH.cpp
@@ -64,45 +64,151 @@ inline std::string GetFilePath(RelationName rn, DataSize ds)
 	return datapath[ds] + filename[rn];
 }
 
-void run_Q3(DataSize ds, bool printResult)
+
+void run_Q3(DataSize ds, bool printResult, bool resultProtection, bool dualExecution)
 {
-	vector<string> o_groupBy = {"o_orderkey", "o_orderdate", "o_shippriority"};
-	auto cust_ri = GetRI(CUSTOMER, Q3, ds, SERVER);
-	Relation::AnnotInfo cust_ai = {true, true};
-	Relation customer(cust_ri, cust_ai);
-	auto filePath = GetFilePath(CUSTOMER, ds);
-	customer.LoadData(filePath.c_str(), "q3_annot");
+	if(!dualExecution){
+		vector<string> o_groupBy = {"o_orderkey", "o_orderdate", "o_shippriority"};
+		auto cust_ri = GetRI(CUSTOMER, Q3, ds, SERVER);
+		Relation::AnnotInfo cust_ai = {true, true};
+		Relation customer(cust_ri, cust_ai);
+		auto filePath = GetFilePath(CUSTOMER, ds);
+		customer.LoadData(filePath.c_str(), "q3_annot");
 
-	auto orders_ri = GetRI(ORDERS, Q3, ds, CLIENT);
-	Relation::AnnotInfo orders_ai = {true, true};
-	Relation orders(orders_ri, orders_ai);
-	filePath = GetFilePath(ORDERS, ds);
-	orders.LoadData(filePath.c_str(), "q3_annot");
-	//orders.Print();
+		auto orders_ri = GetRI(ORDERS, Q3, ds, CLIENT);
+		Relation::AnnotInfo orders_ai = {true, true};
+		Relation orders(orders_ri, orders_ai);
+		filePath = GetFilePath(ORDERS, ds);
+		orders.LoadData(filePath.c_str(), "q3_annot");
+		//orders.Print();
 
-	auto lineitem_ri = GetRI(LINEITEM, Q3, ds, SERVER);
-	Relation::AnnotInfo lineitem_ai = {false, true};
-	Relation lineitem(lineitem_ri, lineitem_ai);
-	filePath = GetFilePath(LINEITEM, ds);
-	lineitem.LoadData(filePath.c_str(), "q3_annot");
-	lineitem.Aggregate();
+		auto lineitem_ri = GetRI(LINEITEM, Q3, ds, SERVER);
+		Relation::AnnotInfo lineitem_ai = {false, true};
+		Relation lineitem(lineitem_ri, lineitem_ai);
+		filePath = GetFilePath(LINEITEM, ds);
+		lineitem.LoadData(filePath.c_str(), "q3_annot");
+		lineitem.Aggregate();
 
-	orders.SemiJoin(customer, "o_custkey", "c_custkey");
-	//orders.PrintTableWithoutRevealing("orders semijoin customer");
-	// PSI: 95.4267ms
-	// Two OEPs: 53.8459ms
-	// AnnotMul: 4.4265ms
+		orders.SemiJoin(customer, "o_custkey", "c_custkey");
+		//orders.PrintTableWithoutRevealing("orders semijoin customer");
+		// PSI: 95.4267ms
+		// Two OEPs: 53.8459ms
+		// AnnotMul: 4.4265ms
 
-	orders.SemiJoin(lineitem, "o_orderkey", "l_orderkey");
-	//orders.PrintTableWithoutRevealing("orders semijoin lineitem");
-	// PSI: 162.877ms
-	// Two OEPs: 25.8395ms
-	// AnnotMul: 51.1201ms
+		orders.SemiJoin(lineitem, "o_orderkey", "l_orderkey");
+		//orders.PrintTableWithoutRevealing("orders semijoin lineitem");
+		// PSI: 162.877ms
+		// Two OEPs: 25.8395ms
+		// AnnotMul: 51.1201ms
 
-	orders.Aggregate(o_groupBy);
-	orders.RevealAnnotToOwner();
-	if (printResult)
-		orders.Print();
+		orders.Aggregate(o_groupBy);
+
+		if (printResult){
+			if(resultProtection){
+				orders.RevealAnnotToOwner();
+				orders.Print_Avg_ResultProtection("AVG(orders.annotation)");
+			}
+			else{
+				orders.RevealAnnotToOwner();
+				orders.Print();
+			}
+		}
+	}
+	else{
+		vector<string> o_groupBy = {"o_orderkey", "o_orderdate", "o_shippriority"};
+
+		auto s_cust_ri = GetRI(CUSTOMER, Q3, ds, SERVER);
+		Relation::AnnotInfo s_cust_ai = {true, true};
+		Relation s_customer(s_cust_ri, s_cust_ai);
+		auto s_customer_filePath = GetFilePath(CUSTOMER, ds);
+		s_customer.LoadData(s_customer_filePath.c_str(), "q3_annot");
+
+		auto c_cust_ri = GetRI(CUSTOMER, Q3, ds, CLIENT);
+		Relation::AnnotInfo c_cust_ai = {true, true};
+		Relation c_customer(c_cust_ri, c_cust_ai);
+		auto c_customer_filePath = GetFilePath(CUSTOMER, ds);
+		c_customer.LoadData(c_customer_filePath.c_str(), "q3_annot");
+
+		auto c_orders_ri = GetRI(ORDERS, Q3, ds, CLIENT);
+		Relation::AnnotInfo c_orders_ai = {true, true};
+		Relation c_orders(c_orders_ri, c_orders_ai);
+		auto c_orders_filePath = GetFilePath(ORDERS, ds);
+		c_orders.LoadData(c_orders_filePath.c_str(), "q3_annot");
+		//orders.Print();
+
+		auto s_orders_ri = GetRI(ORDERS, Q3, ds, SERVER);
+		Relation::AnnotInfo s_orders_ai = {true, true};
+		Relation s_orders(s_orders_ri, s_orders_ai);
+		auto s_orders_filePath = GetFilePath(ORDERS, ds);
+		s_orders.LoadData(s_orders_filePath.c_str(), "q3_annot");
+
+		auto s_lineitem_ri = GetRI(LINEITEM, Q3, ds, SERVER);
+		Relation::AnnotInfo s_lineitem_ai = {false, true};
+		Relation s_lineitem(s_lineitem_ri, s_lineitem_ai);
+		auto s_lineitem_filePath = GetFilePath(LINEITEM, ds);
+		s_lineitem.LoadData(s_lineitem_filePath.c_str(), "q3_annot");
+		s_lineitem.Aggregate();
+
+		auto c_lineitem_ri = GetRI(LINEITEM, Q3, ds, CLIENT);
+		Relation::AnnotInfo c_lineitem_ai = {false, true};
+		Relation c_lineitem(c_lineitem_ri, c_lineitem_ai);
+		auto c_lineitem_filePath = GetFilePath(LINEITEM, ds);
+		c_lineitem.LoadData(c_lineitem_filePath.c_str(), "q3_annot");
+		c_lineitem.Aggregate();
+
+		c_orders.SemiJoin(s_customer, "o_custkey", "c_custkey");
+
+		c_orders.SemiJoin(s_lineitem, "o_orderkey", "l_orderkey");
+
+		c_orders.Aggregate(o_groupBy);
+
+		s_orders.SemiJoin(c_customer, "o_custkey", "c_custkey");
+
+		s_orders.SemiJoin(c_lineitem, "o_orderkey", "l_orderkey");
+
+		s_orders.Aggregate(o_groupBy);
+
+		if(printResult){
+			c_orders.RevealAnnotToOwner();
+			s_orders.RevealAnnotToOwner();
+			bool verified = true;
+			std::vector<uint64_t> packedTuples;
+			if (c_orders.IsDummy())
+			{
+				packedTuples = s_orders.PackTuples();
+				gParty.Send(packedTuples);
+			}
+			else
+			{
+				gParty.Recv(packedTuples);
+				std::vector<uint64_t> c_packedTuples = c_orders.PackTuples();
+				for (uint32_t i = 0; i < c_packedTuples.size(); i++)
+				{
+					if (c_packedTuples[i] != packedTuples[i])
+					{
+						cout << "Result verification failed! Abort!" << endl;
+						verified = false;
+						break;
+					}
+				}
+			}
+			if (verified){
+				if(resultProtection){
+					cout << "c_orders.Print_Avg_ResultProtection(\"AVG(orders.annotation)\")" << endl;
+					c_orders.Print_Avg_ResultProtection("AVG(orders.annotation)");
+					cout << "s_orders.Print_Avg_ResultProtection(\"AVG(orders.annotation)\")" << endl;
+					s_orders.Print_Avg_ResultProtection("AVG(orders.annotation)");
+				}
+				else{
+					cout << "c_orders.Print()" << endl;
+					c_orders.Print();
+					cout << "s_orders.Print()" << endl;
+					s_orders.Print();
+				}
+				cout << "Result verification passed!" << endl;
+			}
+		}
+	}
 }
 
 void run_Q10(DataSize ds, bool printResult)
